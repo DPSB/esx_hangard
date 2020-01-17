1f735fcdbdd4053aa942615210097da780b91d66
rex2630 <rex2630@gmail.com>
2020-01-17 23:30:05 +0100

    Minor cleanup

--- esx_hangard/server/main.lua
+++ esx_hangard/server/main.lua
@@ -1,21 +1,18 @@
-ESX              = nil
+ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 AddEventHandler('esx:playerLoaded', function(source)
-
-  local xPlayer = ESX.GetPlayerFromId(source)
-    
+  local xPlayer = ESX.GetPlayerFromId(source) 
 end)
-
 
 ESX.RegisterServerCallback('esx_hangard:getOwnedVehicles', function (source, cb)
   local xPlayer = ESX.GetPlayerFromId(source)
 
-  MySQL.Async.fetchAll(
-    'SELECT * FROM owned_vehicles WHERE owner = @owner',
-    { ['@owner'] = xPlayer.identifier },
-    function (result)
+  MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE owner = @owner', {
+	['@owner'] = xPlayer.identifier
+  }, function (result)
+
       local vehicles = {}
 
       for i=1, #result, 1 do
@@ -24,20 +21,18 @@ ESX.RegisterServerCallback('esx_hangard:getOwnedVehicles', function (source, cb)
       end
 
       cb(vehicles)
-    end
-  )
+    end)
 end)
 
 ESX.RegisterServerCallback('esx_hangard:checkIfVehicleIsOwned', function (source, cb, plate)
-
   local xPlayer = ESX.GetPlayerFromId(source)
   local found = nil
   local vehicleData = nil
 
-  MySQL.Async.fetchAll(
-    'SELECT * FROM owned_vehicles WHERE owner = @owner',
-    { ['@owner'] = xPlayer.identifier },
-    function (result)
+  MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE owner = @owner', {
+	['@owner'] = xPlayer.identifier
+  }, function (result)
+
       local vehicles = {}
 
       for i=1, #result, 1 do
@@ -52,22 +47,17 @@ ESX.RegisterServerCallback('esx_hangard:checkIfVehicleIsOwned', function (source
       if not found then
         cb(nil)
       end
-    end
-  )
+    end)
 end)
 
 RegisterServerEvent('esx_hangard:updateOwnedVehicle')
 AddEventHandler('esx_hangard:updateOwnedVehicle', function(vehicleProps)
- 
  	local _source = source
  	local xPlayer = ESX.GetPlayerFromId(source)
  
- 	MySQL.Async.fetchAll(
- 		'SELECT * FROM owned_vehicles WHERE owner = @owner',
- 		{
+ 	MySQL.Async.fetchAll('SELECT * FROM owned_vehicles WHERE owner = @owner', {
  			['@owner'] = xPlayer.identifier
- 		},
- 		function(result)
+ 		}, function(result)
  
  			local foundVehicleId = nil
  
@@ -79,102 +69,70 @@ AddEventHandler('esx_hangard:updateOwnedVehicle', function(vehicleProps)
  					foundVehicleId = result[i].id
  					break
  				end
- 
  			end
  
  			if foundVehicleId ~= nil then
 
- 				MySQL.Async.execute(
- 					'UPDATE owned_vehicles SET vehicle = @vehicle WHERE id = @id',
- 					{
+ 				MySQL.Async.execute('UPDATE owned_vehicles SET vehicle = @vehicle WHERE id = @id', {
 						['@vehicle'] = json.encode(vehicleProps),
 						['@id']      = foundVehicleId
  					}
  				)
- 
  			end
- 
- 		end
- 	)
- 
+ 		end)
  end)
-
 
 RegisterServerEvent('esx_hangard:addCarToParking')
 AddEventHandler('esx_hangard:addCarToParking', function(vehicleProps)
-
 	local xPlayer = ESX.GetPlayerFromId(source)
 
 	if vehicleProps ~= nil then
 
-		MySQL.Async.execute(
-			'INSERT INTO `user_hangard` (`identifier`, `plate`, `vehicle`) VALUES (@identifier, @plate, @vehicle)',
-			{
+		MySQL.Async.execute('INSERT INTO `user_hangard` (`identifier`, `plate`, `vehicle`) VALUES (@identifier, @plate, @vehicle)', {
 				['@identifier']   = xPlayer.identifier,
-        ['@plate']        = vehicleProps.plate,
+        			['@plate']        = vehicleProps.plate,
 				['@vehicle']      = json.encode(vehicleProps)
 			}, function(rowsChanged)
 				TriggerClientEvent('esx:showNotification', xPlayer.source, _U('veh_stored'))
-			end
-		)
-
+			end)
 	end
-
 end)
 
 RegisterServerEvent('esx_hangard:removeCarFromParking')
 AddEventHandler('esx_hangard:removeCarFromParking', function(plate)
-
 	local xPlayer = ESX.GetPlayerFromId(source)
 
 	if plate ~= nil then
 
-		MySQL.Async.execute(
-			'DELETE FROM `user_hangard` WHERE `identifier` = @identifier AND `plate` = @plate',
-			{
+		MySQL.Async.execute('DELETE FROM `user_hangard` WHERE `identifier` = @identifier AND `plate` = @plate', {
 				['@identifier'] = xPlayer.identifier,
-        ['@plate'] = plate
+        			['@plate'] = plate
 			}, function(rowsChanged)
 				TriggerClientEvent('esx:showNotification', xPlayer.source, _U('veh_released'))
-			end
-		)
-
+			end)
 	end
-
 end)
-
 
 RegisterServerEvent('esx_hangard:getCustomPlate')
 AddEventHandler('esx_hangard:getCustomPlate', function(plate)
-
 	local xPlayer = ESX.GetPlayerFromId(source)
 
 	if plate ~= nil then
 
-    MySQL.Async.fetchAll(
-      'SELECT `plate_name` FROM custom_plate WHERE original_plate = @plate',
-      {
+    MySQL.Async.fetchAll('SELECT `plate_name` FROM custom_plate WHERE original_plate = @plate', {
         ['@plate'] = plate
-      },
-      function(result)
+      }, function(result)
         return result[1].plate_name
     end)
-
 	end
-
 end)
 
-
 ESX.RegisterServerCallback('esx_hangard:getVehiclesInGarage', function(source, cb)
-
 	local xPlayer = ESX.GetPlayerFromId(source)
 
-	MySQL.Async.fetchAll(
-		'SELECT * FROM `user_hangard` WHERE `identifier` = @identifier',
-		{
+	MySQL.Async.fetchAll('SELECT * FROM `user_hangard` WHERE `identifier` = @identifier', {
 			['@identifier'] = xPlayer.identifier
-		},
-		function(result)
+		}, function(result)
 
 			local vehicles = {}
 
@@ -184,10 +142,7 @@ ESX.RegisterServerCallback('esx_hangard:getVehiclesInGarage', function(source, c
 			end
 
 			cb(vehicles)
-
-		end
-	)
-
+		end)
 end)
 
 --[[ runs everytime the server is restarted]]
